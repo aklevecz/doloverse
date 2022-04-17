0dc6dfee442c347a71a89d7733039d3302d57ca0
aklevecz <teh@raptor.pizza>
2022-04-16 18:38:49 -0700

    egg metadata test

--- app/pages/api/metadata/[tokenId].ts
+++ app/pages/api/metadata/[tokenId].ts
@@ -3,28 +3,39 @@ import { HOST } from "../../../constants";
 
 import DoloverseDb from "../../../lib/doloverseDb";
 
+const TICKET_TOKENID_MAX = 40;
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { tokenId } = req.query;
   const dolodb = new DoloverseDb();
   const cleanTokenId = (tokenId as string).replace(".json", "");
+  const cleanTokenIdInt = parseInt(cleanTokenId);
   let ticket = await dolodb.queryTicketByTString(cleanTokenId as string);
   if (!ticket) {
-    ticket = await dolodb.queryTicketByTokenId(parseInt(cleanTokenId));
+    ticket = await dolodb.queryTicketByTokenId(cleanTokenIdInt);
   }
-  if (ticket) {
-    if (!ticket.hatched) {
-      ticket.name = `Doloverse Ticket ${cleanTokenId}`;
-      ticket.description = `This is your ticket to Doloverse. Once it is checked in at the festival grounds it will take a new form.`;
-      ticket.image = `${HOST}/bw-bao.png`;
-    } else {
-      ticket.name = `Doloverse Transformed Ticket ${cleanTokenId}`;
-      ticket.description = `This is your ticket to Doloverse. You are checked in and ready to enjoy the show!`;
-      ticket.image = `${HOST}/roll_vid.mp4`;
-      ticket.animation_url = `${HOST}/roll.mp4`;
+  if (cleanTokenIdInt <= 40) {
+    if (ticket) {
+      if (!ticket.hatched) {
+        ticket.name = `Doloverse Ticket ${cleanTokenId}`;
+        ticket.description = `This is your ticket to Doloverse. Once it is checked in at the festival grounds it will take a new form.`;
+        ticket.image = `${HOST}/bw-bao.png`;
+      } else {
+        ticket.name = `Doloverse Transformed Ticket ${cleanTokenId}`;
+        ticket.description = `This is your ticket to Doloverse. You are checked in and ready to enjoy the show!`;
+        ticket.image = `${HOST}/roll_vid.mp4`;
+        ticket.animation_url = `${HOST}/roll.mp4`;
+      }
     }
-
-    console.log(ticket);
-    res.json(ticket);
+  } else if (cleanTokenIdInt > 40) {
+    if (ticket) {
+      // query the egg from markers
+      ticket.name = "Egg";
+      ticket.description = "This is an egg";
+      ticket.image = "https://assets.doloverse.com/daisy.png";
+    }
   }
+  res.json(ticket);
+
   res.status(404).end();
 };
